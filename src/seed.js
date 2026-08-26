0a780652d298138997598f4ce877022d561cca8d
antonioamandio <antonioamandio22@gmail.com>
2026-08-26 22:51:23 +0100

    feat: add user generation function and logging setup in seed script

--- src/seed.js
+++ src/seed.js
@@ -14,3 +14,20 @@ if (
     console.error('The amount must be a whole number greater than zero.')
     process.exit(1)
 }
+
+const stream = createWriteStream(LOG_FILE)
+
+function generateFakeUser() {
+    return {
+        ip: faker.internet.ip(),
+        username: faker.internet.userName(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        location: faker.location.city(),
+        jobArea: faker.name.jobArea(),
+        company: faker.company.name(),
+        jobTitle: faker.name.jobTitle(),
+        id: faker.string.uuid(),
+    }
+}
